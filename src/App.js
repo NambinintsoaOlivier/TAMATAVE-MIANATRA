37703863b89e6a79ffb7b351533b4ebb5cb59746
Nambinintsoa <nambinintsoaolivier25@gmail.com>
2024-01-30 16:03:18 +0300

    creation du projet tamatave mianatra REACT JS

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
