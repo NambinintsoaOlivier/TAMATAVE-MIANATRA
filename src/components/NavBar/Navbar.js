86c1df5ff8e196794267243fd7106230cfb0f801
Nambinintsoa <nambinintsoaolivier25@gmail.com>
2024-01-30 17:01:49 +0300

    Ajout du navbar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,9 +1,50 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+        <div class="container-fluid d-flex justify-content-end">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarCollapse"
+          >
+            <ul class="navbar-nav mb-2 mb-md-0">
+              <li class="nav-item">
+                <a
+                  class="nav-link custom-nav-link active"
+                  aria-current="page"
+                  href="/"
+                >
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link custom-nav-link" href="/">
+                  Formations
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link custom-nav-link" href="/">
+                  Se connecter
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
