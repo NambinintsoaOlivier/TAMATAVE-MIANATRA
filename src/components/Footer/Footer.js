6bb0d7718034dfa14918206e14b7312ce54bfe31
Nambinintsoa <nambinintsoaolivier25@gmail.com>
2024-01-30 17:36:03 +0300

    mis à jours du Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,15 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="container-fluid">
       <footer
-        className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
+        className="d-flex flex-wrap justify-content-end align-items-center py-4 my-4 border-top"
         style={{ background: "#101010", color: "#EDC254" }}
       >
         <div className="col-md-4 d-flex align-items-center">
-          <a href="/" className="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
-            <svg className="bi" width="30" height="24">
-              <use xlinkHref="#bootstrap" />
-            </svg>
-          </a>
-          <span>&copy; 2024 Company, Inc</span>
+          <span className="mr-3">&copy; 2024 Company, Inc</span>
         </div>
       </footer>
     </div>
