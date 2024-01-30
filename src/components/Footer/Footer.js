8090b0e5208b3a9c06ca3ce5d06dcbe71dcdebc6
Nambinintsoa <nambinintsoaolivier25@gmail.com>
2024-01-30 19:51:11 +0300

    creation page home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,16 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="container-fluid">
-      <footer
+    <footer className="container-fluid mt-auto">
+      <div
         className="d-flex flex-wrap justify-content-end align-items-center py-4 my-4 border-top"
         style={{ background: "#101010", color: "#EDC254" }}
       >
         <div className="col-md-4 d-flex align-items-center">
           <span className="mr-3">&copy; 2024 Company, Inc</span>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,29 @@
 import React from "react";
+import "../styles/Home.css";
+import logoHome from "../styles/images/logoTamMian.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <main>
-        <div className="container">
-          <h1>Home</h1>
+        <div className="container-fluid">
+          <div className="home-content ">
+            <div className="home-content-item d-flex flex-column align-items-center">
+              <img src={logoHome} alt="" />
+              <div className="home-menu d-flex flex-column align-items-center justify-content-center">
+                <Link to={""} className="btn btn-search">
+                  Search
+                </Link>
+                <Link to={""} className="btn btn-login mt-3">
+                  Login
+                </Link>
+                <Link to={""} className="btn btn-create-account mt-3">
+                  Create account
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
