69f9a33ff87f13fff6d664522ade63b94547a48d
Nambinintsoa <nambinintsoaolivier25@gmail.com>
2024-01-31 06:58:58 +0300

    mis à jour page home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,15 +11,16 @@ const Home = () => {
           <div className="home-content ">
             <div className="home-content-item d-flex flex-column align-items-center">
               <img src={logoHome} alt="" />
+              <p>En cours de developpement...</p>
               <div className="home-menu d-flex flex-column align-items-center justify-content-center">
                 <Link to={""} className="btn btn-search">
-                  Search
+                  Trouver du formation
                 </Link>
                 <Link to={""} className="btn btn-login mt-3">
-                  Login
+                  Se connecter
                 </Link>
                 <Link to={""} className="btn btn-create-account mt-3">
-                  Create account
+                  Créer un compte
                 </Link>
               </div>
             </div>
